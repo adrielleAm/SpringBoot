79d41e86333c6b26b4ee96a3a568ea622546b7dc
adrielladde aad <adrielle-am@outlook.com>
2019-09-03 19:37:47 -0300

    Primeiro Commit

---
+++ src/main/java/com/maquinadebusca/app/repository/DocumentoRepository.java
@@ -0,0 +1,10 @@
+package com.maquinadebusca.app.repository;
+
+import com.maquinadebusca.app.model.Documento;
+
+public interface DocumentoRepository {
+
+	public static Documento iniciar() {
+		return null;
+	}
+}
